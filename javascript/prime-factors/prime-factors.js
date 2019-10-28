bc8eb1048e18a04ae88caba32b80a1e89a562636
Matt Lehrer <lehrerm@gmail.com>
2019-10-28 14:39:44 -0700

    delete line from refactor left by mistake

--- javascript/prime-factors/prime-factors.js
+++ javascript/prime-factors/prime-factors.js
@@ -2,9 +2,7 @@ export const primeFactors = num => {
   let primeFactors = [];
   let i = 2;
   let remainder = num;
-
-  if (remainder )
-
+  
   while (remainder >= 2) {
     if (remainder % i) {
       i++;
