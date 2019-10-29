06766e70a3f1d8405a8285966b25688c0b308576
Matt Lehrer <lehrerm@gmail.com>
2019-10-29 10:39:12 -0700

    DRY change based on mentor feedback

--- javascript/space-age/space-age.js
+++ javascript/space-age/space-age.js
@@ -12,17 +12,17 @@
 const EARTH_YEAR_IN_SECONDS = 31557600;
 
 const EARTH_YEAR_CONVERSION = {
-  'mercury': 0.2408467 * EARTH_YEAR_IN_SECONDS,
-  'venus': 0.61519726 * EARTH_YEAR_IN_SECONDS,
-  'earth': 1.0 * EARTH_YEAR_IN_SECONDS,
-  'mars': 1.8808158 * EARTH_YEAR_IN_SECONDS,
-  'jupiter': 11.862615 * EARTH_YEAR_IN_SECONDS,
-  'saturn': 29.447498 * EARTH_YEAR_IN_SECONDS,
-  'uranus': 84.016846 * EARTH_YEAR_IN_SECONDS,
-  'neptune': 164.79132 * EARTH_YEAR_IN_SECONDS
+  'mercury': 0.2408467,
+  'venus': 0.61519726,
+  'earth': 1.0,
+  'mars': 1.8808158,
+  'jupiter': 11.862615,
+  'saturn': 29.447498,
+  'uranus': 84.016846,
+  'neptune': 164.79132
 };
 
 export const age = (planet, ageInSeconds) => {
-  const ageInYears = ageInSeconds / EARTH_YEAR_CONVERSION[planet];
+  const ageInYears = ageInSeconds / (EARTH_YEAR_CONVERSION[planet] * EARTH_YEAR_IN_SECONDS);
   return Number(ageInYears.toFixed(2));
 };
