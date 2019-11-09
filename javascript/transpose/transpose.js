931eda57900a72aec6120f03790b9c02fccd4d89
Matt Lehrer <lehrerm@gmail.com>
2019-11-09 13:45:22 -0800

    transpose passes all tests

---
+++ javascript/transpose/transpose.js
@@ -0,0 +1,16 @@
+export const transpose = rows => {
+  if (!rows.length) return [];
+  // find max length row which becomes the number of output rows
+  const maxRow = rows.reduce((max, curr, j) => rows[max].length > curr.length ? max : j, 0);
+  const numberOfNewRows = rows[maxRow].length;
+  const numberOfNewColumns = rows.length;
+  let cols = [];
+  for (let i = 0; i < numberOfNewRows; i++) {
+    let col = rows.reduce((acc, curr) => {
+      return `${acc}${curr[i] || ' '}`;
+    }, '');
+    if (i === numberOfNewRows - 1) col = col.trimEnd();
+    cols.push(col);
+  }
+  return cols;
+};
