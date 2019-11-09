bb41ce4eab055cd76b5df0150ccdd9c987c9c221
Matt Lehrer <lehrerm@gmail.com>
2019-11-09 13:49:26 -0800

    remove unused variable

--- javascript/transpose/transpose.js
+++ javascript/transpose/transpose.js
@@ -3,7 +3,6 @@ export const transpose = rows => {
   // find max length row which becomes the number of output rows
   const maxRow = rows.reduce((max, curr, j) => rows[max].length > curr.length ? max : j, 0);
   const numberOfNewRows = rows[maxRow].length;
-  const numberOfNewColumns = rows.length;
   let cols = [];
   for (let i = 0; i < numberOfNewRows; i++) {
     let col = rows.reduce((acc, curr) => {
