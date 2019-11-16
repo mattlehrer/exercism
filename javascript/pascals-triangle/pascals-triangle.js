832d13489eedab30bea23722753b333c21c1310e
Matt Lehrer <lehrerm@gmail.com>
2019-11-16 15:56:02 -0800

    parameter named more explicitly

--- javascript/pascals-triangle/pascals-triangle.js
+++ javascript/pascals-triangle/pascals-triangle.js
@@ -1,8 +1,8 @@
 export class Triangle {
-  constructor(rows) {
-    this.last = rows;
+  constructor(numRows) {
+    this.last = numRows;
     let triangle = [[1]];
-    for (let i = 1; i < rows; i++) {
+    for (let i = 1; i < numRows; i++) {
       let row = [];
       for (let j = 0; j <= i; j++) {
         row.push((triangle[i-1][j-1] || 0) + (triangle[i-1][j] || 0));
