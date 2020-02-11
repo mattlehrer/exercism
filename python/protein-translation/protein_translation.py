4ac8c65031da9830aaea9d80ea36256ed35875cb
Matt Lehrer <lehrerm@gmail.com>
2020-02-11 13:57:14 -0800

    passes all tests

--- python/protein-translation/protein_translation.py
+++ python/protein-translation/protein_translation.py
@@ -1,2 +1,29 @@
+codons = {
+  'AUG': 'Methionine',
+  'UUU': 'Phenylalanine',
+  'UUC': 'Phenylalanine',
+  'UUA': 'Leucine',
+  'UUG': 'Leucine',
+  'UCU': 'Serine',
+  'UCC': 'Serine',
+  'UCA': 'Serine',
+  'UCG': 'Serine',
+  'UAU': 'Tyrosine',
+  'UAC': 'Tyrosine',
+  'UGU': 'Cysteine',
+  'UGC': 'Cysteine',
+  'UGG': 'Tryptophan',
+  'UAA': 'STOP',
+  'UAG': 'STOP',
+  'UGA': 'STOP'
+}
+
 def proteins(strand):
-    pass
+  output = []
+  i = 0
+  while ( i + 3 <= len(strand)):
+    if codons[strand[i:i+3]] == 'STOP':
+      return output
+    output.append(codons[strand[i:i+3]])
+    i += 3
+  return output
