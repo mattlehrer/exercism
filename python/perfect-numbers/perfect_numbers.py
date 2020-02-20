125d30b2d18ca6bad89826f23b00ebdbb0052d44
Matt Lehrer <lehrerm@gmail.com>
2020-02-20 15:51:13 -0800

    passes all tests

--- python/perfect-numbers/perfect_numbers.py
+++ python/perfect-numbers/perfect_numbers.py
@@ -1,2 +1,19 @@
 def classify(number):
-    pass
+  if number <= 0:
+    raise ValueError('Not a natural number')
+  factors = []
+  if number > 1:
+    factors.append(1)
+  i = 2
+  while i <= number / 2:
+    if number % i == 0:
+      factors.append(i)
+    i += 1
+  total = sum(factors)
+  print(number, factors, sum(factors))
+  if total == number:
+    return 'perfect'
+  elif total < number:
+    return 'deficient'
+  else:
+    return 'abundant'
