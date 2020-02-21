6f97cb79f4fb37c8b1c4267ff9f46fbd436f85fb
Matt Lehrer <lehrerm@gmail.com>
2020-02-21 11:51:45 -0800

    passes all tests

--- python/series/series.py
+++ python/series/series.py
@@ -1,2 +1,4 @@
 def slices(series, length):
-    pass
+  if length < 1 or length > len(series):
+    raise ValueError('Invalid length')
+  return [series[i:i+length] for i in range(0, 1+len(series) - length)]
