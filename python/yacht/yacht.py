605857fc94be80084a96941f9e466c4f56172290
Matt Lehrer <lehrerm@gmail.com>
2020-02-21 12:52:42 -0800

    passes all tests

--- python/yacht/yacht.py
+++ python/yacht/yacht.py
@@ -14,19 +14,19 @@ You can learn more here: https://en.wikipedia.org/wiki/Enumerated_type
 
 # Score categories.
 # Change the values as you see fit.
-YACHT = None
-ONES = None
-TWOS = None
-THREES = None
-FOURS = None
-FIVES = None
-SIXES = None
-FULL_HOUSE = None
-FOUR_OF_A_KIND = None
-LITTLE_STRAIGHT = None
-BIG_STRAIGHT = None
-CHOICE = None
+YACHT = lambda d: 50 if len(set(d)) == 1 else 0
+ONES = lambda d: d.count(1)
+TWOS = lambda d: 2 * d.count(2)
+THREES = lambda d: 3 * d.count(3)
+FOURS = lambda d: 4 * d.count(4)
+FIVES = lambda d: 5 * d.count(5)
+SIXES = lambda d: 6 * d.count(6)
+FULL_HOUSE = lambda d: sum(d) if len(set(d)) == 2 and (d.count(d[0]) == 2 or d.count(d[0]) == 3) else 0
+FOUR_OF_A_KIND = lambda d: 4 * d[0] if d.count(d[0]) >= 4 else 0 + 4 * d[1] if d.count(d[1]) >= 4 and d[0] != d[1] else 0
+LITTLE_STRAIGHT = lambda d: 30 if len(set(d)) == 5 and 6 not in d else 0
+BIG_STRAIGHT = lambda d: 30 if len(set(d)) == 5 and 1 not in d else 0
+CHOICE = lambda d: sum(d)
 
 
 def score(dice, category):
-    pass
+  return category(dice)
