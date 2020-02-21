50f3716790d07d8149c6b4ed1298573fc0ff08c9
Matt Lehrer <lehrerm@gmail.com>
2020-02-21 12:10:01 -0800

    passes all tests

--- python/rna-transcription/rna_transcription.py
+++ python/rna-transcription/rna_transcription.py
@@ -1,6 +1,3 @@
 def to_rna(dna_strand):
   COMPLEMENT = {'G': 'C', 'C': 'G', 'T': 'A', 'A': 'U'}
-  rna = []
-  for char in dna_strand:
-    rna.append(COMPLEMENT[char])
-  return ''.join(rna)
+  return ''.join(map(lambda c: COMPLEMENT[c], dna_strand))
