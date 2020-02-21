972051d75ecb8957ff17db05b7d882701d88492a
Matt Lehrer <lehrerm@gmail.com>
2020-02-21 11:52:49 -0800

    simplify range()

--- python/series/series.py
+++ python/series/series.py
@@ -1,4 +1,4 @@
 def slices(series, length):
   if length < 1 or length > len(series):
     raise ValueError('Invalid length')
-  return [series[i:i+length] for i in range(0, 1+len(series) - length)]
+  return [series[i:i+length] for i in range(1 + len(series) - length)]
