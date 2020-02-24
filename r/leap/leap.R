c7146e69006b83c722564b190556d9288cf1df8b
Matt Lehrer <lehrerm@gmail.com>
2020-02-24 12:35:50 -0800

    one liner passes all tests

--- r/leap/leap.R
+++ r/leap/leap.R
@@ -1,10 +1,3 @@
 leap <- function(year) {
-  if ( year %% 400 == 0 ) {
-    return (TRUE)
-  } else if ( year %% 100 == 0 ) {
-    return (FALSE)
-  } else if ( year %% 4 == 0 ) {
-    return (TRUE)
-  } else
-    return (FALSE)
+  year %% 400 == 0 || (year %% 100 != 0 & year %% 4 == 0)
 }
