60ac04ad7f9ff43b5689d6585076f78599c13b7c
Matt Lehrer <lehrerm@gmail.com>
2020-02-25 12:35:35 -0800

    fix indentation

--- python/resistor-color/resistor_color.py
+++ python/resistor-color/resistor_color.py
@@ -1,15 +1,15 @@
 CODE = {
-    'black': 0,
-    'brown': 1,
-    'red': 2,
-    'orange': 3,
-    'yellow': 4,
-    'green': 5,
-    'blue': 6,
-    'violet': 7,
-    'grey': 8,
-    'white': 9
-  }
+  'black': 0,
+  'brown': 1,
+  'red': 2,
+  'orange': 3,
+  'yellow': 4,
+  'green': 5,
+  'blue': 6,
+  'violet': 7,
+  'grey': 8,
+  'white': 9
+}
   
 def color_code(color):
   return CODE[color]
