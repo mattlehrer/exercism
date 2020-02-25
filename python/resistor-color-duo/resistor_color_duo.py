1240aa85b775011689fa0ee4597933b774dbd152
Matt Lehrer <lehrerm@gmail.com>
2020-02-25 12:37:40 -0800

    passes all tests

--- python/resistor-color-duo/resistor_color_duo.py
+++ python/resistor-color-duo/resistor_color_duo.py
@@ -1,2 +1,15 @@
+CODE = {
+  'black': 0,
+  'brown': 1,
+  'red': 2,
+  'orange': 3,
+  'yellow': 4,
+  'green': 5,
+  'blue': 6,
+  'violet': 7,
+  'grey': 8,
+  'white': 9
+}
+
 def value(colors):
-    pass
+  return CODE[colors[0]]*10 + CODE[colors[1]]
