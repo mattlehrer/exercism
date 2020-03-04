2c86d502c1a662c3007c247bdfd15f5feb901b2d
Matt Lehrer <lehrerm@gmail.com>
2020-03-04 15:08:05 -0800

    passes all tests

--- typescript/resistor-color-duo/resistor-color-duo.ts
+++ typescript/resistor-color-duo/resistor-color-duo.ts
@@ -1,8 +1,28 @@
 export class ResistorColor {
-  private colors: string[];
-
-  constructor(colors: string[]) {
-    this.colors = colors;
-  }
-  value = (): number => 0;
+	private colors: string[];
+	private CONVERSION = [
+		'black',
+		'brown',
+		'red',
+		'orange',
+		'yellow',
+		'green',
+		'blue',
+		'violet',
+		'grey',
+		'white',
+	];
+	constructor(colors: string[]) {
+		if (colors.length < 2) {
+			throw new Error('At least two colors need to be present');
+		}
+		this.colors = colors;
+	}
+	value = (): number => {
+		return Number(
+			`${this.CONVERSION.indexOf(this.colors[0])}${this.CONVERSION.indexOf(
+				this.colors[1],
+			)}`,
+		);
+	};
 }
