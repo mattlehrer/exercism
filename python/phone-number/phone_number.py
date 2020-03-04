ae116f8e0b897217cc878c24724ddeb4712f155b
Matt Lehrer <lehrerm@gmail.com>
2020-03-04 10:45:08 -0800

    passes all tests

--- python/phone-number/phone_number.py
+++ python/phone-number/phone_number.py
@@ -1,3 +1,28 @@
+import re
+
 class PhoneNumber:
     def __init__(self, number):
-        pass
+        regex = r"\d+"
+        matches = re.findall(regex, number)
+        num = ''
+        for match in matches:
+            num += str(match)
+        if num[0] == '1':
+            num = num[1:]
+        if len(num) != 10 or num[0] == '1' or num[0] == '0' or num[3] == '1' or num[3] == '0':
+            raise ValueError('Invalid number')
+        self.__num = num
+        self.__area_code = num[:3]
+        self.__exchange = num[3:6]
+        self.__last_four = num[6:]
+
+    @property
+    def number(self):
+        return self.__num
+
+    @property
+    def area_code(self):
+        return self.__num[:3]
+
+    def pretty(self):
+        return "({}) {}-{}".format(self.__area_code, self.__exchange, self.__last_four)
