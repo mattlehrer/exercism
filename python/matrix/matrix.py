5b8a0da2634f959acb419d0ead311e86a6681275
Matt Lehrer <lehrerm@gmail.com>
2020-03-03 17:08:49 -0800

    passes all tests

--- python/matrix/matrix.py
+++ python/matrix/matrix.py
@@ -1,9 +1,15 @@
 class Matrix:
     def __init__(self, matrix_string):
-        pass
+        r_strings = list(map(lambda a: a.split(' '), matrix_string.split('\n')))
+        self.m = []
+        for r in r_strings:
+            self.m.append(list(map(int, r)))
 
     def row(self, index):
-        pass
+        return self.m[index-1]
 
     def column(self, index):
-        pass
+        c = []
+        for r in self.m:
+            c.append(r[index-1])
+        return c
