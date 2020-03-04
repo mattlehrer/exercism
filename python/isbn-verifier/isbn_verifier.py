7983d91eae2624b051d1cabcee2214d72ae93729
Matt Lehrer <lehrerm@gmail.com>
2020-03-04 11:09:12 -0800

    passes all tests

--- python/isbn-verifier/isbn_verifier.py
+++ python/isbn-verifier/isbn_verifier.py
@@ -1,2 +1,18 @@
+import re
+
+def value(d):
+    if d == 'X':
+        return 10
+    if d.isdigit():
+        return int(d)
+    raise ValueError('Invalid digit')
+
 def is_valid(isbn):
-    pass
+    matches = re.findall(r"[\dX]+", isbn)
+    clean = ''.join(matches)
+    if len(clean) != 10 or 0 <= clean.find('X') < 9:
+        return False
+    total = 0
+    for i, v in enumerate([value(d) for d in clean]):
+        total += v * (10-i)
+    return total % 11 == 0
