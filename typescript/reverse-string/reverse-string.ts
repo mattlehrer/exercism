4ce901dc12d6dc56ab110ae23da6020d0de199a8
Matt Lehrer <lehrerm@gmail.com>
2020-03-05 15:58:36 -0800

    passes all tests

--- typescript/reverse-string/reverse-string.ts
+++ typescript/reverse-string/reverse-string.ts
@@ -1,7 +1,13 @@
 class ReverseString {
-    static reverse( /* Parameters go here */ ) {
-        // Your code here
-    }
+	static reverse(str: string): string {
+		let str2 = '';
+		let i = str.length;
+		while (i >= 0) {
+			str2 += str.charAt(i);
+			i -= 1;
+		}
+		return str2;
+	}
 }
 
-export default ReverseString
+export default ReverseString;
