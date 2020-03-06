8bd6beac3d9cbe33df6b963e4a612e062f138aca
Matt Lehrer <lehrerm@gmail.com>
2020-03-06 12:43:55 -0800

    passes all tests

--- typescript/triangle/triangle.ts
+++ typescript/triangle/triangle.ts
@@ -1,12 +1,25 @@
 export default class Triangle {
+	sides: number[];
 
-    sides: number[]
+	constructor(...sides: number[]) {
+		this.sides = sides;
+	}
 
-    constructor(...sides: number[]) {
-        this.sides = sides
-    }
-
-    kind() {
-        return ""
-    }
+	kind(): string {
+		const a: number = this.sides[0];
+		const b: number = this.sides[1];
+		const c: number = this.sides[2];
+		if (
+			this.sides.length != 3 ||
+			this.sides.some(s => s <= 0) ||
+			a + b < c ||
+			c + b < a ||
+			a + c < b
+		) {
+			throw new Error('Invalid sides');
+		}
+		if (a === b && a === c) return 'equilateral';
+		if (a === b || a === c || b === c) return 'isosceles';
+		return 'scalene';
+	}
 }
