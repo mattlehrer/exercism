3fdca112178c1f285111e8e5dc580d3e948ab694
Matt Lehrer <lehrerm@gmail.com>
2020-03-06 14:27:11 -0800

    init

---
+++ typescript/raindrops/raindrops.ts
@@ -0,0 +1,15 @@
+export default class Raindrops {
+	static convert(n: number): string {
+		let output = '';
+		if (!(n % 3)) {
+			output += 'Pling';
+		}
+		if (!(n % 5)) {
+			output += 'Plang';
+		}
+		if (!(n % 7)) {
+			output += 'Plong';
+		}
+		return output ? output : String(n);
+	}
+}
