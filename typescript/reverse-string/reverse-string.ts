5e48d6dc5487a0900d2da2765eb7b97c84dcb0e6
Matt Lehrer <lehrerm@gmail.com>
2020-03-05 16:09:38 -0800

    using array methods

--- typescript/reverse-string/reverse-string.ts
+++ typescript/reverse-string/reverse-string.ts
@@ -1,12 +1,6 @@
 class ReverseString {
 	static reverse(str: string): string {
-		let str2 = '';
-		let i = str.length;
-		while (i >= 0) {
-			str2 += str.charAt(i);
-			i -= 1;
-		}
-		return str2;
+		return [...str].reverse().join('');
 	}
 }
 
