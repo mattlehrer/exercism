1cada1cfdf3a8c6841ec74268ed3217af2d15c62
Matt Lehrer <lehrerm@gmail.com>
2020-03-06 13:24:13 -0800

    passes all tests

--- typescript/collatz-conjecture/collatz-conjecture.ts
+++ typescript/collatz-conjecture/collatz-conjecture.ts
@@ -1,7 +1,13 @@
 class CollatzConjecture {
-    static steps( /* Parameters go here */ ) {
-        // Your code here
-    }
+	static steps(n: number): number {
+		// Take any positive integer n.
+		if (n <= 0) throw new Error('Only positive numbers are allowed');
+		if (n === 1) return 0;
+		// If n is odd, multiply n by 3 and add 1 to get 3n + 1.
+		if (n % 2) return 1 + this.steps(n * 3 + 1);
+		// If n is even, divide n by 2 to get n / 2.
+		return 1 + this.steps(n / 2);
+	}
 }
 
-export default CollatzConjecture
+export default CollatzConjecture;
