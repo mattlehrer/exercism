36d094536ee86cd6594ca6ed7da25fc78d1716d3
Matt Lehrer <lehrerm@gmail.com>
2020-03-06 13:29:58 -0800

    passes all tests

--- typescript/protein-translation/protein-translation.ts
+++ typescript/protein-translation/protein-translation.ts
@@ -1,7 +1,52 @@
 class ProteinTranslation {
-    static proteins( /* Parameters go here */ ) {
-        // Your code here
-    }
+	static proteins(rna: string = ''): string[] {
+		let proteins: string[] = [];
+		const codons = rna.match(/[A-Z]{3}/g);
+		if (codons === null) {
+			return proteins;
+		}
+		let i = 0;
+		while (i < codons.length) {
+			switch (codons[i]) {
+				case 'AUG':
+					proteins.push('Methionine');
+					break;
+				case 'UUU':
+				case 'UUC':
+					proteins.push('Phenylalanine');
+					break;
+				case 'UUA':
+				case 'UUG':
+					proteins.push('Leucine');
+					break;
+				case 'UCU':
+				case 'UCC':
+				case 'UCA':
+				case 'UCG':
+					proteins.push('Serine');
+					break;
+				case 'UAU':
+				case 'UAC':
+					proteins.push('Tyrosine');
+					break;
+				case 'UGU':
+				case 'UGC':
+					proteins.push('Cysteine');
+					break;
+				case 'UGG':
+					proteins.push('Tryptophan');
+					break;
+				case 'UAA':
+				case 'UAG':
+				case 'UGA':
+					return proteins;
+				default:
+					throw new Error('Invalid codon');
+			}
+			i += 1;
+		}
+		return proteins;
+	}
 }
 
-export default ProteinTranslation
+export default ProteinTranslation;
