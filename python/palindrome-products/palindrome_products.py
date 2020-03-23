4fe6f38bdc4812e6a9acfb5a6cb734a47a48184c
Matt Lehrer <lehrerm@gmail.com>
2020-03-23 14:21:12 -0700

    passes all tests

--- python/palindrome-products/palindrome_products.py
+++ python/palindrome-products/palindrome_products.py
@@ -1,6 +1,39 @@
-def largest(min_factor, max_factor):
-    pass
+from math import floor, ceil
+
+def factors(n, min_factor, max_factor):
+    if n == 1:
+        return [[1, 1]]
+    return [[i, int(n/i)] for i in range(min_factor, floor(n**0.5) + 1) if not n % i and n/i <= max_factor]
 
 
-def smallest(min_factor, max_factor):
-    pass
+def products(min_factor, max_factor):
+    return {i*j for i in range(min_factor, max_factor + 1) for j in range(min_factor, max_factor + 1)}
+
+
+def is_palindrome(n):
+    str_n = str(n)
+    return all(str_n[i] == str_n[-1 - i] for i in range(ceil(len(str_n))))
+
+
+def palindromes(numbers):
+    return {n for n in numbers if is_palindrome(n)}
+
+
+def largest(max_factor, min_factor = 0):
+    if max_factor < min_factor:
+        raise ValueError('Invalid')
+    pals = palindromes(products(min_factor, max_factor))
+    if pals:
+        value = max(pals)
+        return value, factors(value, min_factor, max_factor)
+    return None, []
+
+
+def smallest(max_factor, min_factor = 0):
+    if max_factor <= min_factor:
+        raise ValueError('Invalid')
+    pals = palindromes(products(min_factor, max_factor))
+    if pals:
+        value = min(pals)
+        return value, factors(value, min_factor, max_factor)
+    return None, []
