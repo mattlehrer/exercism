64457c834feb49d59869704b4e0646c523ba945b
Matt Lehrer <lehrerm@gmail.com>
2020-03-23 10:41:44 -0700

    passes all tests

--- python/grains/grains.py
+++ python/grains/grains.py
@@ -1,6 +1,9 @@
 def square(number):
-    pass
+    if not 1 <= number <= 64:
+        raise ValueError('Out of range')
+    return 2 ** (number - 1)
 
 
 def total():
-    pass
+    # return sum(2**(n - 1) for n in range(1, 65))
+    return 2**64 - 1
