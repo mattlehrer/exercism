238368c34a9389f37efb280b8911a4550fa6e81f
Matt Lehrer <lehrerm@gmail.com>
2020-03-22 23:01:40 -0700

    refactor to comprehension

--- python/difference-of-squares/difference_of_squares.py
+++ python/difference-of-squares/difference_of_squares.py
@@ -3,10 +3,7 @@ def square_of_sum(number):
 
 
 def sum_of_squares(number):
-    output = 0
-    for i in range(1, number + 1):
-        output += i ** 2
-    return output
+    return sum(i ** 2 for i in range(1, number + 1))
 
 
 def difference_of_squares(number):
