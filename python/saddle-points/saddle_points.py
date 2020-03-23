6c9a89136b12951bc6b6dba10eaa97ca2013a585
Matt Lehrer <lehrerm@gmail.com>
2020-03-23 12:47:20 -0700

    refactor to set comprehension

--- python/saddle-points/saddle_points.py
+++ python/saddle-points/saddle_points.py
@@ -1,12 +1,12 @@
 def saddle_points(matrix):
     if not matrix:
         return []
-    row_lens = [len(row) for row in matrix]
-    if max(row_lens) != min(row_lens):
+    row_lens = {len(row) for row in matrix}
+    if len(row_lens) != 1:
         raise ValueError('Invalid matrix')
-    points = []
     row_max = [max(row) for row in matrix]
     col_min = [min(col) for col in [[row[i] for row in matrix] for i in range(len(matrix[0]))]]
+    points = []
     for row in range(len(matrix)):
         for col in range(len(matrix[0])):
             value = matrix[row][col]
