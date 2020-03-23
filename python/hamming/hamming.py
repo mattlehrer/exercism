86b1890d305e93a0c4513bee868f3adf4414de84
Matt Lehrer <lehrerm@gmail.com>
2020-03-22 22:42:14 -0700

    passes all tests

--- python/hamming/hamming.py
+++ python/hamming/hamming.py
@@ -1,2 +1,8 @@
 def distance(strand_a, strand_b):
-    pass
+    if len(strand_a) != len(strand_b):
+        raise ValueError('Invalid')
+    dist = 0
+    for i, c in enumerate(strand_a):
+        if c != strand_b[i]:
+            dist += 1
+    return dist
