3769d68f7ee29e758bfa321bbfce2bc0b744c453
Matt Lehrer <lehrerm@gmail.com>
2020-03-23 12:38:10 -0700

    passes all tests

--- python/saddle-points/saddle_points.py
+++ python/saddle-points/saddle_points.py
@@ -1,2 +1,15 @@
 def saddle_points(matrix):
-    pass
+    if not matrix:
+        return []
+    row_lens = [len(row) for row in matrix]
+    if max(row_lens) != min(row_lens):
+        raise ValueError('Invalid matrix')
+    points = []
+    row_max = [max(row) for row in matrix]
+    col_min = [min(col) for col in [[row[i] for row in matrix] for i in range(len(matrix[0]))]]
+    for row in range(len(matrix)):
+        for col in range(len(matrix[0])):
+            value = matrix[row][col]
+            if value == row_max[row] and value == col_min[col]:
+                points.append({"row": row + 1, "column": col + 1})
+    return points
