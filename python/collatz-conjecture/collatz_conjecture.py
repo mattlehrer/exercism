f05eed049078d12109c210afab7eefb11c7613e9
Matt Lehrer <lehrerm@gmail.com>
2020-03-24 13:41:44 -0700

    passes all tests

--- python/collatz-conjecture/collatz_conjecture.py
+++ python/collatz-conjecture/collatz_conjecture.py
@@ -1,2 +1,6 @@
 def steps(number):
-    pass
+    if number < 1:
+        raise ValueError('Invalid')
+    if number == 1:
+        return 0
+    return 1 + (steps(3*number + 1) if number % 2 else steps(number / 2))
