059b9652a71e7009d4353dddcf374190a4a69e39
Matt Lehrer <lehrerm@gmail.com>
2020-03-24 13:34:02 -0700

    passes all tests

--- python/triangle/triangle.py
+++ python/triangle/triangle.py
@@ -1,10 +1,10 @@
 def equilateral(sides):
-    pass
+    return len({s for s in sides}) == 1 and sides[0] != 0
 
 
 def isosceles(sides):
-    pass
+    return len({s for s in sides}) <= 2 and all(s != 0 and sum(sides) - 2*s > 0 for s in sides)
 
 
 def scalene(sides):
-    pass
+    return len({s for s in sides}) == 3 and all(s != 0 and sum(sides) - 2*s > 0 for s in sides)
