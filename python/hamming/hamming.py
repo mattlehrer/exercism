16c1bb910fc0dcc4b64737c58060cdc0700c1cbd
Matt Lehrer <lehrerm@gmail.com>
2020-04-05 17:46:20 -0700

    use zip!

--- python/hamming/hamming.py
+++ python/hamming/hamming.py
@@ -1,8 +1,4 @@
 def distance(strand_a, strand_b):
     if len(strand_a) != len(strand_b):
         raise ValueError('Invalid')
-    dist = 0
-    for i, c in enumerate(strand_a):
-        if c != strand_b[i]:
-            dist += 1
-    return dist
+    return sum([a != b for a, b in zip(strand_a, strand_b)])
