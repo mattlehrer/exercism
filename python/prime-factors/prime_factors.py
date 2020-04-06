26336fcbfbaa6d675c3d8bd3fc42064f79f43049
Matt Lehrer <lehrerm@gmail.com>
2020-04-05 18:30:47 -0700

    passes all tests

--- python/prime-factors/prime_factors.py
+++ python/prime-factors/prime_factors.py
@@ -1,2 +1,15 @@
 def factors(value):
-    pass
+    if value == 1:
+        return []
+    factor_list = []
+    current = value
+    while not current % 2:
+        factor_list.append(2)
+        current = int(current/2)
+    divisor = 3
+    while divisor <= current:
+        while not current % divisor:
+            current = int(current/divisor)
+            factor_list.append(divisor)
+        divisor += 2
+    return factor_list
