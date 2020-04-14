42e704ad4ffc5e18e472a4f32f79ba3688716d15
Matt Lehrer <lehrerm@gmail.com>
2020-04-14 10:10:48 -0700

    refactor

--- python/isogram/isogram.py
+++ python/isogram/isogram.py
@@ -1,2 +1,5 @@
 def is_isogram(string):
-    return not len({c.lower() for c in string if c not in ' -'}) + len([c for c in string if c in ' -']) - len(string)
+  non_letters = ' -'
+  num_letters = len({c.lower() for c in string if c not in non_letters})
+  num_non_letters = len([c for c in string if c in non_letters])
+  return num_letters + num_non_letters == len(string)
